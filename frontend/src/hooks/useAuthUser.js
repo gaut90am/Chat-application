337bd96102a8b5b34600f849854f004108e20dce
Gautam Kumar <csefromnitp2026@gmail.com>
2025-09-02 01:36:56 +0530

    Changed auth user hook

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -3,12 +3,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getAuthUser } from "../lib/api";
 
 const useAuthUser = () => {
+  const token = localStorage.getItem("token"); // or your auth check
+
   const authUser = useQuery({
     queryKey: ["authUser"],
     queryFn: getAuthUser,
-    retry: false, // auth check
+    enabled: !!token, // only run query if token exists
+    retry: false,
   });
 
   return { isLoading: authUser.isLoading, authUser: authUser.data?.user };
 };
+
 export default useAuthUser;

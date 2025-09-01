42e1b768317c46236c1b7f2f03481fa6a3f27724
Gautam Kumar <csefromnitp2026@gmail.com>
2025-09-02 00:44:12 +0530

    Updated frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://chat-application-mtpc.onrender.com";
+const BASE_URL = "https://chat-application-mtpc.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,

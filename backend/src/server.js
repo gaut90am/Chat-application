20377ca349ddc302e61786ab630a692051b8f587
Gautam Kumar <csefromnitp2026@gmail.com>
2025-09-02 00:35:53 +0530

    Updated Backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,43 @@
-
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-import cors from "cors";
-import {connectDB} from "./lib/db.js";
-import cookieParser from "cookie-parser";
+import chatRoutes from "./routes/chat.route.js";
 
-// Load environment variables from .env file
-dotenv.config();
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT ; // Fallback to 5001 if .env doesn't have PORT
+const PORT = process.env.PORT;
 
-// 
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true // allow frontend to send cookies
-}));
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRoutes);
-app.use("/api/auth",userRoutes);
 
-// Start server
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
-
